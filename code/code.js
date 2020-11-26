06e0b07a16293699395a81463f2a7e91bbe4f357
Anshul Pandey <nagendra194276@gmail.com>
2020-11-26 17:11:34 +0530

    IDE Running for C,C++,Java,Python,Python3

--- code/code.js
+++ code/code.js
@@ -1,22 +1,3 @@
-function addDefault(){
-    var template = `#include <bits/stdc++.h>
-using namespace std;
-    
-int main(){
-  return 0;
-}`;
-    return template;
-}
-
-function setTabKey(editor){
-    editor.setOption("extraKeys", {
-        Tab: function(cm) {
-          var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
-          cm.replaceSelection(spaces);
-        }
-    });
-}
-
 function sleep(milliseconds) {
     const date = Date.now();
     let currentDate = null;
@@ -25,42 +6,80 @@ function sleep(milliseconds) {
     } while (currentDate - date < milliseconds);
 }
 
+function changeClass1(team_id){
+    team_c.classList.remove("active");
+    team_cpp.classList.remove("active");
+    team_java.classList.remove("active");
+    team_python.classList.remove("active");
+    team_python3.classList.remove("active");
 
-// A function to choose mode must be inserted
+    team_id.classList.add("active");
+}
 
-// Also the mode must be choosen for display and running explicitly
+function changeClass2(personal_id){
+    personal_c.classList.remove("active");
+    personal_cpp.classList.remove("active");
+    personal_java.classList.remove("active");
+    personal_python.classList.remove("active");
+    personal_python3.classList.remove("active");
 
-var code1 = document.getElementById("team_code");
-var code2 = document.getElementById("personal_code");
-code1.value = addDefault();
-code2.value = addDefault();
-var editor1 = CodeMirror.fromTextArea(document.getElementById("team_code"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    autoCloseBrackets: true,
-    mode: "text/x-c++src"
-});
-var editor2 = CodeMirror.fromTextArea(document.getElementById("personal_code"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    autoCloseBrackets: true,
-    mode: "text/x-c++src"
-});
+    personal_id.classList.add("active");
+}
 
-//setTabKey(editor1);
-setTabKey(editor2);
+function initialize_code(){
+    templates.push(`#include <stdio.h>
 
+int main() {
+  // your code goes here
+  return 0;
+}
+    `);
+    templates.push(`#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+  // your code goes here
+  return 0;
+}
+    `);
+    templates.push(`import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-//                                  Running the code
+/* Name of the class has to be "Main" only if the class is public. */
+class Code
+{
+  public static void main (String[] args) throws java.lang.Exception{
+	// your code goes here
+  }
+}
+    `);
+    templates.push(`# your code goes here`);
+    templates.push(`# your code goes here`);
+}
 
+function initialize_mode(){
+    modes.push("text/x-c++src");
+    modes.push("text/x-c++src");
+    modes.push("text/x-c++src");
+    modes.push("text/x-python");
+    modes.push("text/x-python");
+}
 
-// IDE Access Parameters (Need to be changed every 14 days)
-var endpoint = '5f7b9d68.compilers.sphere-engine.com';
-var accessToken = '18d67ae37ef25dcb576ade7d6e9ebf03';
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const mainUrl = 'https://' + endpoint + '/api/v4/submissions?';
+function initialize_compiler(){
+    compilers.push(11);
+    compilers.push(44);
+    compilers.push(10);
+    compilers.push(4);
+    compilers.push(116);
+}
 
+//Method used to set compilerId, compilerMode and default language code
+function addDefault(editor, type){
+    editor.setOption('mode', modes[type]);
+    editor.setValue(templates[type]);
+    compiler = compilers[type];
+}
 
 function get_output(submissionId, output){
     const url = proxyUrl + 'https://' + endpoint + '/api/v4/submissions/' + submissionId + '?access_token=' + accessToken;
@@ -90,11 +109,12 @@ function get_output(submissionId, output){
 }
 
 function run_compile(editor, input, output){
-    var compiler = 41;
     var source_code = editor.getValue();
     var input_data = document.getElementById(input).value;
+    var running = document.getElementById(output);
+    running.value = "Code is running -----";
     const url = proxyUrl + mainUrl;
-    fetch(url +new URLSearchParams({
+    fetch(url + new URLSearchParams({
         access_token: accessToken.toString(),
         compilerId: compiler.toString(),
         source: source_code.toString(),
@@ -110,3 +130,57 @@ function run_compile(editor, input, output){
     )
     .catch(() => console.log("Some ERROR! Happened"));
 }
+
+//Team's Code nav variables
+var team_c = document.getElementById("c_1");
+var team_cpp = document.getElementById("c++_1");
+var team_java = document.getElementById("java_1");
+var team_python = document.getElementById("python_1");
+var team_python3 = document.getElementById("python3_1");
+
+//Personal Code nav variables
+var personal_c = document.getElementById("c_2");
+var personal_cpp = document.getElementById("c++_2");
+var personal_java = document.getElementById("java_2");
+var personal_python = document.getElementById("python_2");
+var personal_python3 = document.getElementById("python3_2");
+
+
+
+var templates=[];
+initialize_code(); //To define the templates for some programming languages
+
+var modes=[];
+initialize_mode();
+
+var compilers=[];
+initialize_compiler();
+
+var code1 = document.getElementById("team_code");
+var code2 = document.getElementById("personal_code");
+
+var editor1 = CodeMirror.fromTextArea(code1 , {
+    lineNumbers: true,
+    matchBrackets: true,
+    autoCloseBrackets: true,
+    tabSize: 2
+});
+var editor2 = CodeMirror.fromTextArea(code2 , {
+    lineNumbers: true,
+    matchBrackets: true,
+    autoCloseBrackets: true,
+    tabSize: 2
+});
+
+addDefault(editor1, 0);
+addDefault(editor2, 0);
+
+//                                  Running the code
+
+// IDE Access Parameters (Need to be changed every 14 days)
+var endpoint = '5f7b9d68.compilers.sphere-engine.com';
+var accessToken = '18d67ae37ef25dcb576ade7d6e9ebf03';
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const mainUrl = 'https://' + endpoint + '/api/v4/submissions?';
+
+var compiler = compilers[0];
