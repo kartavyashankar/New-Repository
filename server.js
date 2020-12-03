275004c0e4c12f5cbdab492e96c314e02600a76c
Kartavya Shankar <kartavyashankar@gmail.com>
2020-12-03 22:06:12 +0530

    Corrections for Heroku

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ io.on('connection', socket => {
 	});
 });
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
